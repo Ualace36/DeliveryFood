49b74c43f46723b53a824c3f3c4a89ad637dc3bd
Ualace36 <ualace26@gmail.com>
2023-08-07 23:26:38 -0300

    create project spring

---
+++ delivery-food/src/main/java/com/delivery/deliveryfood/DeliveryFoodApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.deliveryfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryFoodApplication.class, args);
+	}
+
+}
