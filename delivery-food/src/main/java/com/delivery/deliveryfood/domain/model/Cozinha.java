0298f27c8f0439e96eafee55dbfa7568e5a3cfb8
Ualace36 <ualace26@gmail.com>
2023-08-17 21:46:37 -0300

    modify id as autoincrement

--- delivery-food/src/main/java/com/delivery/deliveryfood/domain/model/Cozinha.java
+++ delivery-food/src/main/java/com/delivery/deliveryfood/domain/model/Cozinha.java
@@ -1,16 +1,15 @@
 package com.delivery.deliveryfood.domain.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "tab_cozinhas")
 public class Cozinha {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(name = "nom_cozinha")
 	private String nome;
 	public Long getId() {
 		return id;
