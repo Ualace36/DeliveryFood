0ab959e116d8f73ca833a694a1c0944ed9d7804a
Ualace36 <ualace26@gmail.com>
2023-08-31 00:22:36 -0300

    Features: Add FasterXml in pom.xml and create singleton Resource buscar(Long cozinhaId)

--- delivery-food/src/main/java/com/delivery/deliveryfood/api/controller/CozinhaController.java
+++ delivery-food/src/main/java/com/delivery/deliveryfood/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,9 @@ public class CozinhaController {
 	public List<Cozinha> lista() {
 		return cozinhaRepository.listar();
 	}
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable("cozinhaId") Long id) {
+    	return cozinhaRepository.buscar(id);
+    }
 	
 }
