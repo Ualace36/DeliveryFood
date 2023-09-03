ae8428959193964a39470e287d91542ba26fc327
Ualace36 <ualace26@gmail.com>
2023-09-03 10:42:36 -0300

    Feat: implement of the method Post in CozinhaController

--- delivery-food/src/main/java/com/delivery/deliveryfood/api/controller/CozinhaController.java
+++ delivery-food/src/main/java/com/delivery/deliveryfood/api/controller/CozinhaController.java
@@ -7,8 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.delivery.deliveryfood.domain.model.Cozinha;
@@ -29,7 +32,16 @@ public class CozinhaController {
     @GetMapping("/{cozinhaId}")
     public ResponseEntity<Cozinha>  buscar(@PathVariable("cozinhaId") Long id) {
     	Cozinha cozinha = cozinhaRepository.buscar(id);
-		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+    	if(cozinha != null) {
+    		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+    	}
+    	//return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    	return ResponseEntity.notFound().build();
     }
 	
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+    	return cozinhaRepository.salvar(cozinha);
+    }
 }
