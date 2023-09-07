bc285dd38c5708937353b3ff152b2712cdf5462a
Ualace36 <ualace26@gmail.com>
2023-09-06 21:59:48 -0300

    Feat : Refactoring CozinhaController.java

--- delivery-food/src/main/java/com/delivery/deliveryfood/api/controller/CozinhaController.java
+++ delivery-food/src/main/java/com/delivery/deliveryfood/api/controller/CozinhaController.java
@@ -61,7 +61,7 @@ public class CozinhaController {
 		if(cozinhaAtual != null) {
 			//cozinhaAtual.setNome(cozinha.getNome());
 			 BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
-		    cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+		    cozinhaAtual = cadastroCozinha.salvar(cozinhaAtual);
 	    	return ResponseEntity.ok(cozinhaAtual);
 		}
 		return ResponseEntity.notFound().build();
